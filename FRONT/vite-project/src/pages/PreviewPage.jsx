d37137a174ac9879236f36cab99953a85cc35a68
vegam91 <vegamartinoj@gmail.com>
2023-08-29 20:55:31 +0200

    start: css responsive at AccessLayout PreviewPage

--- FRONT/vite-project/src/pages/PreviewPage.jsx
+++ FRONT/vite-project/src/pages/PreviewPage.jsx
@@ -1,22 +1,43 @@
-import { Container, Stack } from "@mui/material"
+import { Container, Stack, Box } from "@mui/material";
 
+function Preview() {
+  return (
+    <Container
+      sx={{
+        border: "solid 1px red",
+        direction: "column",
+        display: "flex",
+        height: "100vh",
+        alignItems: "center",
+        justifyContent: "center",
+     padding:{xs:"0px", sm:"0px"} 
+      }}
+    >
+      <Stack border={"1px solid blue"}
+      width={"100%"}
+      height={"100vh"}
+      spacing={"60px"}
+      paddingBottom={"237px"}
+      padding={"0px"}>
 
-function Preview(){
+        <Box bgcolor="green"
+        padding={{xs:"16px", sm:"24"}}>
+          <div>navbar</div>
+        </Box>
 
-    return(
-        <Container >
-            <Stack alignItems={"flex-start"}>
-             <div>navbar</div>   
-            </Stack>
-        
-        <div>
-           mobile
-        </div>
-        </Container>
-        
-    )
-
-    
+        <Box bgcolor="blue">
+          <div>mobile
+            kasdjlaskjdlaskjdasdjalskjdsakjda
+            sadkjsakdasdjalksjdlaksjdasdj
+            asdjlasdjlaskjdlasjdlasjdlaskjdl
+            lsdklasjdlaksjdlkasjdlksajdlskajdlaskjd
+            asd
+            
+         </div>
+        </Box>
+      </Stack>
+    </Container>
+  );
 }
 
-export default Preview
+export default Preview;
